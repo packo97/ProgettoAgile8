6e446dec6188f6cc51be579ea0ccf9f70a8e1438
packo97 <pasqualegatto97@gmail.com>
2020-12-05 09:44:14 +0100

    Initial commit

---
+++ src/main/java/it/unical/demacs/inf/asd/ProgettoAgile8/ProgettoAgile8Application.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.inf.asd.ProgettoAgile8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoAgile8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoAgile8Application.class, args);
+	}
+
+}
